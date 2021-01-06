beee27c7775acfe39186bc7d3c7a7f536ed7109a
TecBiooo <antony.lorenzelli@etu.unice.fr>
2021-01-06 12:05:45 +0100

    5.3.1.1

--- ExoCarto1/script.js
+++ ExoCarto1/script.js
@@ -1,10 +1,10 @@
-var optionsGeo = {
+var options = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0
 };
 
-function successGeo(pos) {
+function success(pos) {
   var crd = pos.coords;
 
   document.getElementById("lat").innerHTML = 'Latitude : '+crd.latitude;
@@ -14,33 +14,31 @@ function successGeo(pos) {
   document.getElementById("tim").innerHTML = 'TimeStamp : '+crd.timestamp;
 }
 
-function errorGeo(err) {
+function error(err) {
   console.warn('ERREUR ('+err.code+'): '+err.message);
 }
 
-navigator.geolocation.watchPosition(successGeo, errorGeo, optionsGeo);
-
 function deviceOrientationHandler(event){
-  document.getElementById("alp").innerHTML = 'Alpha :'+event.alpha;
-  document.getElementById("bet").innerHTML = 'Beta :'+event.beta;
-  document.getElementById("gam").innerHTML = 'Gamma :'+event.gamma;
+  document.getElementById("alp").innerHTML = 'Alpha : '+event.alpha;
+  document.getElementById("bet").innerHTML = 'Beta : '+event.beta;
+  document.getElementById("gam").innerHTML = 'Gamma : '+event.gamma;
 }
 
 function deviceMotionHandler(event){
-  document.getElementById("rotX").innerHTML = 'Rotation X :'+event.rotationRate.beta;
-  document.getElementById("rotY").innerHTML = 'Rotation Y :'+event.rotationRate.gamma;
-  document.getElementById("rotZ").innerHTML = 'Rotation Z :'+event.rotationRate.alpha;
-  document.getElementById("accX").innerHTML = 'Accélération X :'+event.acceleration.x;
-  document.getElementById("accY").innerHTML = 'Accélération Y :'+event.acceleration.y;
-  document.getElementById("accZ").innerHTML = 'Accélération Z :'+event.acceleration.z;
+  document.getElementById("rotX").innerHTML = 'Rotation X : '+event.rotationRate.beta;
+  document.getElementById("rotY").innerHTML = 'Rotation Y : '+event.rotationRate.gamma;
+  document.getElementById("rotZ").innerHTML = 'Rotation Z : '+event.rotationRate.alpha;
+  document.getElementById("accX").innerHTML = 'Accélération X : '+event.acceleration.x;
+  document.getElementById("accY").innerHTML = 'Accélération Y : '+event.acceleration.y;
+  document.getElementById("accZ").innerHTML = 'Accélération Z : '+event.acceleration.z;
 }
+
+navigator.geolocation.watchPosition(success, error, options);
 
 if (window.DeviceOrientationEvent) {
   window.addEventListener('deviceorientation', deviceOrientationHandler, false);
-  //document.getElementById("doeSupported").innerText = "Supported!";
 }
 
 if (window.DeviceMotionEvent) {
   window.addEventListener('devicemotion', deviceMotionHandler);
-  //setTimeout(stopJump, 3*1000);
 }
