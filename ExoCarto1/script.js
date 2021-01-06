c0ea82db2e019a2edf293017dc3a29966685382e
TecBiooo <antony.lorenzelli@etu.unice.fr>
2021-01-06 10:22:25 +0100

    2.1

---
+++ ExoCarto1/script.js
@@ -0,0 +1,26 @@
+var id;
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Votre position actuelle est :');
+  console.log('Latitude : '+crd.latitude);
+  console.log('Longitude : '+crd.longitude);
+  console.log('La précision est de '+crd.accuracy+' mètres.');
+}
+
+function error(err) {
+  console.warn('ERREUR ('+err.code+'): '+err.message);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+id = navigator.geolocation.watchPosition(success, error, options);
+
+console.log(id);
