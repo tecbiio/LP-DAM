3c80819f00ec4f6a604ed6d671eda6bee71e8514
TecBiooo <antony.lorenzelli@etu.unice.fr>
2021-01-06 11:33:46 +0100

    5.0

--- ExoCarto1/script.js
+++ ExoCarto1/script.js
@@ -1,23 +1,31 @@
-var options = {
+var optionsGeo = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0
 };
 
-function success(pos) {
+function successGeo(pos) {
   var crd = pos.coords;
 
   document.getElementById("lat").innerHTML = 'Latitude : '+crd.latitude;
   document.getElementById("lon").innerHTML = 'Longitude : '+crd.longitude;
-  document.getElementById("acc").innerHTML = 'Précision : '+crd.accuracy;
+  document.getElementById("pre").innerHTML = 'Précision : '+crd.accuracy;
   document.getElementById("spe").innerHTML = 'Vitesse : '+crd.speed;
   document.getElementById("tim").innerHTML = 'TimeStamp : '+crd.timestamp;
 }
 
-function error(err) {
+function errorGeo(err) {
   console.warn('ERREUR ('+err.code+'): '+err.message);
 }
 
-//navigator.geolocation.getCurrentPosition(success, error, options);
+navigator.geolocation.watchPosition(successGeo, errorGeo, optionsGeo);
 
-navigator.geolocation.watchPosition(success, error, options);
+if (window.DeviceOrientationEvent) {
+  window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+  document.getElementById("doeSupported").innerText = "Supported!";
+}
+
+if (window.DeviceMotionEvent) {
+  window.addEventListener('devicemotion', deviceMotionHandler);
+  setTimeout(stopJump, 3*1000);
+}
